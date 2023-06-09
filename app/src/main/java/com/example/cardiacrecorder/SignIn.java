10b6c0d631778813890ead9dd7bf4d64e23cf412
Galib35 <adorasadullah001@gmail.com>
2023-06-09 23:16:54 +0600

    signin java

--- app/src/main/java/com/example/cardiacrecorder/SignIn.java
+++ app/src/main/java/com/example/cardiacrecorder/SignIn.java
@@ -1,5 +1,4 @@
 package com.example.cardiacrecorder;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,50 +27,171 @@ import java.util.Objects;
 
 public class SignIn extends AppCompatActivity {
 
-    EditText email, pass;
-    TextInputLayout email_input, pass_input;
+    EditText email,pass;
+    TextInputLayout email_input,pass_input;
     Button btn;
-    TextView create_ac, forget;
+    TextView create_ac,forget;
     ProgressBar pr;
     CheckBox checkBox;
 
     FirebaseAuth auth;
 
-    boolean check = false;
-
+    boolean check=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        create_ac = findViewById(R.id.crt_account);
-        email = findViewById(R.id.email_sign_in);
-        pass = findViewById(R.id.pass_sign_in);
-        email_input = findViewById(R.id.email_input_signin);
-        pass_input = findViewById(R.id.pass_input_signin);
-        btn = findViewById(R.id.sign_in_btn);
-        pr = findViewById(R.id.pr_sign_in);
-        checkBox = findViewById(R.id.cb);
-        forget = findViewById(R.id.fp_txt);
+        create_ac=findViewById(R.id.crt_account);
+        email=findViewById(R.id.email_sign_in);
+        pass=findViewById(R.id.pass_sign_in);
+        email_input=findViewById(R.id.email_input_signin);
+        pass_input=findViewById(R.id.pass_input_signin);
+        btn=findViewById(R.id.sign_in_btn);
+        pr=findViewById(R.id.pr_sign_in);
+        checkBox=findViewById(R.id.cb);
+        forget=findViewById(R.id.fp_txt);
 
-        auth = FirebaseAuth.getInstance();
+        auth=FirebaseAuth.getInstance();
 
-        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
-        boolean remember = preferences.getBoolean("rem", false);
+        SharedPreferences preferences=getSharedPreferences("checkbox",MODE_PRIVATE);
+        boolean remember=preferences.getBoolean("rem",false);
 
-        if (remember) {
-            Intent intent = new Intent(SignIn.this, MainActivity.class);
+        if(remember)
+        {
+            Intent intent=new Intent(SignIn.this,MainActivity.class);
             startActivity(intent);
         }
 
         checkBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                check = checkBox.isChecked();
+                check=checkBox.isChecked();
             }
         });
 
+        create_ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+    //            Intent intent =new Intent(SignIn.this,SignUp.class);
+    //            startActivity(intent);
+            }
+        });
+
+        email.addTextChangedListener(watcher);
+        pass.addTextChangedListener(watcher);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pr.setVisibility(View.VISIBLE);
+                signIn();
+            }
+        });
+
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignIn.this, com.example.cardiacrecorder.ForgetPassword.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    TextWatcher watcher=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            String password,em;
+            em=email.getText().toString().trim();
+            password=pass.getText().toString().trim();
+
+            btn.setEnabled(!password.isEmpty() && !em.isEmpty());
+
+            switch (com.example.cardiacrecorder.Constant.ch)
+            {
+                case 'e':
+                    email_input.setError(null);
+                    break;
+                case 'p':
+                    pass_input.setError(null);
+                    break;
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    void signIn()
+    {
+        String password,em;
+        em=email.getText().toString().trim();
+        password=pass.getText().toString().trim();
+
+        com.example.cardiacrecorder.Constant.ch='n';
+        if(!Patterns.EMAIL_ADDRESS.matcher(em).matches())
+        {
+            pr.setVisibility(View.INVISIBLE);
+            email_input.setError("Invalid email");
+            com.example.cardiacrecorder.Constant.ch='e';
+        }
+        else if(password.contains(" "))
+        {
+            pr.setVisibility(View.INVISIBLE);
+            pass_input.setError("No space is allowed!");
+            com.example.cardiacrecorder.Constant.ch='p';
+        }
+        else if(password.length()<6)
+        {
+            pr.setVisibility(View.INVISIBLE);
+            pass_input.setError("Must contain at least 6 character");
+            com.example.cardiacrecorder.Constant.ch='p';
+        }
+
+        else
+        {
+            auth.signInWithEmailAndPassword(em,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        pr.setVisibility(View.INVISIBLE);
+                        Toast.makeText(SignIn.this,"Sign in Successful!",Toast.LENGTH_SHORT).show();
+                        SharedPreferences preferences=getSharedPreferences("checkbox",MODE_PRIVATE);
+                        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor edit=preferences.edit();
+                        edit.putBoolean("rem",check);
+                        edit.apply();
+
+                        Intent intent=new Intent(SignIn.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        pr.setVisibility(View.INVISIBLE);
+                        Toast.makeText(SignIn.this, Objects.requireNonNull(task.getException()).getMessage(),Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(getIntent().getBooleanExtra("from_main",false) || getIntent().getBooleanExtra("from_fp",false))
+        {
+            finishAffinity();
+        }
+        else
+            super.onBackPressed();
 
     }
 }
-
